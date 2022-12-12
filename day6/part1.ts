d823e94cc78c50e5cae44a2c69b393aa042aefd2
joshwaiam <joshwaiam@gmail.com>
2022-12-11 21:28:32 -0500

    Completed day 6, part 1

---
+++ day6/part1.ts
@@ -0,0 +1,43 @@
+import { input, tests } from './input'
+
+function start(datastream: string) {
+  let packetStart = -1
+  let characters = ''
+
+  for (let i = 0; i < datastream.length; i++) {
+    // grab 4-letter chunk, splitting letters into an array
+    const candidate = datastream.slice(i, i + 4).split('')
+
+    let validCandidate = true
+    for (let j = 0; j < candidate.length; j++) {
+      const letter = candidate[j]
+      // check if letter found anywhere else in the 4-letters
+      // if candidate index and j are the same, then it's the
+      // exact same character slot and should not count as a duplicate
+      const isDuplicate =
+        candidate.findIndex((c, index) => c === letter && index !== j) > -1
+
+      if (isDuplicate) {
+        validCandidate = false
+        break
+      }
+    }
+
+    if (validCandidate) {
+      packetStart = i + 4
+      characters = candidate.join('')
+      break
+    }
+  }
+
+  console.info(
+    `The first start-of-packet marker was detected at character ${packetStart}, characters ${characters}`
+  )
+}
+
+start(tests[0]) // answer is 7
+start(tests[1]) // answer is 5
+start(tests[2]) // answer is 6
+start(tests[3]) // answer is 10
+start(tests[4]) // answer is 11
+start(input)
